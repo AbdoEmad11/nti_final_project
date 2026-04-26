769d35711f98578171570bffd8612006f62476d4
Omarabdelhad <151914356+Omarabdelhad@users.noreply.github.com>
2026-04-26 04:18:00 +0300

    Add files via upload
    app_colors

---
+++ app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+ 
+  static Color secondaryColor = const Color(0xff464555);
+  static Color primaryColor = const Color(0xff4D41DF);
+  static Color blackColor = const Color(0xff1A1B22);
+  static Color grayColor = const Color(0xff6B7280);
+  static Color whiteColor = const Color(0xffFFFFFF);
+}
