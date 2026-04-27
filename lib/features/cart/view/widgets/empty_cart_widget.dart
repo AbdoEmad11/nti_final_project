6c287b49e56f0708315cd19b4faf89a619e8e7f3
AbdoEmad11 <Abdelrahmanemad581@gmail.com>
2026-04-27 19:58:04 +0300

    add empty cart widget in cart features layer

---
+++ lib/features/cart/view/widgets/empty_cart_widget.dart
@@ -0,0 +1,62 @@
+// ============================================
+// lib/features/cart/view/widgets/empty_cart_widget.dart
+// ============================================
+
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../../../../core/theme/app_colors.dart';
+import '../../../../../core/theme/app_text_styles.dart';
+import '../../../../../core/widgets/app_buttons.dart';
+
+class EmptyCartWidget extends StatelessWidget {
+  final VoidCallback? onBrowse;
+
+  const EmptyCartWidget({
+    super.key,
+    this.onBrowse,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: EdgeInsets.all(32.w),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 120.w,
+              height: 120.h,
+              decoration: BoxDecoration(
+                color: AppColors.primaryLight,
+                shape: BoxShape.circle,
+              ),
+              child: Icon(
+                Icons.shopping_cart_outlined,
+                size: 48.sp,
+                color: AppColors.primary,
+              ),
+            ),
+            SizedBox(height: 24.h),
+            Text(
+              'Your Cart is Empty',
+              style: AppTextStyles.headlineMedium,
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 8.h),
+            Text(
+              'Looks like you haven\'t added anything to your cart yet.',
+              style: AppTextStyles.bodyMedium,
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 32.h),
+            AppButton(
+              label: 'Start Shopping',
+              onTap: onBrowse,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
