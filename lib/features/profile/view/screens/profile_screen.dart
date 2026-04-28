c5e1d2b998a64d3dd1e89a99901aa7bb176d372d
AbdoEmad11 <Abdelrahmanemad581@gmail.com>
2026-04-28 17:21:20 +0300

    update profile screen with cubit and app bar widget and delete profile widget

--- lib/features/profile/view/screens/profile_screen.dart
+++ lib/features/profile/view/screens/profile_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../../../core/theme/app_colors.dart';
 import '../../../../core/theme/app_text_styles.dart';
+import '../../../../core/widgets/app_bar_widget.dart';
+import '../../data/cubits/profile_cubit.dart';
 import '../widgets/profile_avatar.dart';
 
 class ProfileScreen extends StatelessWidget {
