dfb28e7abc4e3567c2a4ed532010ea3bee2d5a6a
Medodev-code <alzamalk2030@gmail.com>
2026-04-28 21:14:11 +0300

    add reset password screen in auth feature view layer

---
+++ lib/features/auth/view/screens/reset_password_screen.dart
@@ -0,0 +1,157 @@
+import 'package:final_project/widgets/title_text_filed.dart';
+import 'package:flutter/material.dart';
+
+class ResetPasswordScreen extends StatefulWidget {
+  const ResetPasswordScreen({super.key});
+
+  @override
+  State<ResetPasswordScreen> createState() => _ResetPasswordScreenState();
+}
+
+class _ResetPasswordScreenState extends State<ResetPasswordScreen> {
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController confirmController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffF5F3FF),
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.arrow_back_outlined, color: Color(0xff4D41DF)),
+        ),
+        backgroundColor: Colors.white,
+        title: Text(
+          "Reset Password",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 30),
+
+                Center(
+                  child: Container(
+                    width: 128,
+                    height: 128,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(14),
+                      color: Colors.white,
+                    ),
+                    child: Icon(
+                      Icons.verified_user,
+                      size: 60,
+                      color: Color(0xff4D41DF),
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 20),
+
+                Center(
+                  child: Text(
+                    "Reset Password",
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+
+                SizedBox(height: 10),
+
+                Center(
+                  child: Text(
+                    """Your new password must be \ndifferent from previous""",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Color(0xff464555),
+                      fontWeight: FontWeight.w500,
+                      fontSize: 14,
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 30),
+                TitleTextFiled(
+                  title: "New Password",
+                  myController: passwordController,
+                ),
+                SizedBox(height: 20),
+                TitleTextFiled(
+                  title: "Confirm Password",
+                  myController: confirmController,
+                ),
+
+                SizedBox(height: 30),
+
+                SizedBox(
+                  width: double.infinity,
+                  height: 55,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xff4D41DF),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18),
+                      ),
+                    ),
+                    onPressed: () {
+                      if (passwordController.text != confirmController.text) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text(
+                              "Passwords don't match",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w700,
+                                fontSize: 18,
+                              ),
+                            ),
+                            backgroundColor: Colors.red,
+                          ),
+                        );
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text(
+                              "Password Reset Success",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w700,
+                                fontSize: 18,
+                              ),
+                            ),
+                            backgroundColor: Colors.green,
+                          ),
+                        );
+                      }
+                    },
+                    child: Text(
+                      "Reset Password",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                ),
+
+
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
