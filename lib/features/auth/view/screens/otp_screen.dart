10555283c5e7d96df4978757e63ca2122e3fa980
Medodev-code <alzamalk2030@gmail.com>
2026-04-28 21:12:23 +0300

    add otp verification screen in auth features view layer

---
+++ lib/features/auth/view/screens/otp_screen.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class OtpVerificationScreen extends StatefulWidget {
+  const OtpVerificationScreen({super.key});
+
+  @override
+  State<OtpVerificationScreen> createState() => _OtpVerificationScreenState();
+}
+
+class _OtpVerificationScreenState extends State<OtpVerificationScreen> {
+  List<TextEditingController> controllers = List.generate(
+    6,
+        (_) => TextEditingController(),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffF5F3FF),
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.arrow_back_outlined, color: Color(0xff4D41DF)),
+        ),
+        backgroundColor: Colors.white,
+        title: Text(
+          "L U X E",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 30),
+              Container(
+                width: 128,
+                height: 128,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  color: Colors.white,
+                ),
+                child: Icon(
+                  Icons.verified_user,
+                  size: 60,
+                  color: Color(0xff4D41DF),
+                ),
+              ),
+              SizedBox(height: 20),
+
+              Text(
+                "Verify Your Account",
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xff1A1B22),
+                ),
+              ),
+
+              SizedBox(height: 10),
+
+              Text(
+                "Enter the 6-digit code sent to your email",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  color: Color(0xff464555),
+                ),
+              ),
+
+              SizedBox(height: 10),
+
+              Text(
+                "em***@gmail.com",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700,
+                  color: Color(0xff4D41DF),
+                ),
+              ),
+
+              SizedBox(height: 30),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: List.generate(6, (index) {
+                  return SizedBox(
+                    width: 45,
+                    child: TextFormField(
+                      controller: controllers[index],
+                      keyboardType: TextInputType.number,
+                      textAlign: TextAlign.center,
+                      maxLength: 1,
+
+                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+
+                      decoration: InputDecoration(
+                        counterText: "",
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+
+                      onChanged: (value) {
+                        if (value.isNotEmpty && index < 5) {
+                          FocusScope.of(context).nextFocus();
+                        }
+                      },
+                    ),
+                  );
+                }),
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
