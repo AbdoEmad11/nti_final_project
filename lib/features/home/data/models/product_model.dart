e5a2bf4d873f5523c484861ffe9b0cf2e40ef6f5
Medodev-code <alzamalk2030@gmail.com>
2026-04-28 20:56:24 +0300

    add product model in home feature data layer

---
+++ lib/features/home/data/models/product_model.dart
@@ -0,0 +1,17 @@
+class ProductModel {
+  final String image;
+  final String name;
+  final double price;
+  final double rate;
+  final int id;
+  final int numberOfBuy;
+
+  ProductModel({
+    required this.image,
+    required this.name,
+    required this.price,
+    required this.id,
+    required this.rate,
+    required this.numberOfBuy,
+  });
+}
